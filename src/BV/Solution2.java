268bd61a740534d62453de785620d09d72f6246c
SANAYEE <dudtkd0219@gmail.com>
2022-01-23 15:29:07 +0900

    이상한 프로그래밍 문자열 체크하기

---
+++ src/BV/Solution2.java
@@ -0,0 +1,25 @@
+package BV;
+
+import java.io.*;
+
+class Result2{
+	public static int programmerStrings(String s) {
+	    
+		return 0;
+	}
+}
+
+
+public class Solution2 {
+	public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+       
+        String s = bufferedReader.readLine();
+
+        int result = Result2.programmerStrings(s);
+        
+        System.out.println(result);
+
+        bufferedReader.close();
+    }
+}
